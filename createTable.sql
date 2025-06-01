481d190d74a8a8a232e7e8b9cbbaf13c897b86af
SEIYA_WATANABE\81904 <seiyaprogram007@gmail.com>
2025-06-01 23:34:57 +0900

    createTable編集

--- createTable.sql
+++ createTable.sql
@@ -1,5 +1,6 @@
 -- 家計簿と費目テーブルが存在する場合、削除する
 DROP TABLE IF EXISTS 家計簿;
+DROP TABLE IF EXISTS 家計簿アーカイブ;
 DROP TABLE IF EXISTS 費目;
 
 -- 費目テーブルの作成
@@ -30,3 +31,11 @@ VALUES
 ('2024-04-12', 2, N'家賃',　0, 600000),
 ('2025-05-31', 1, N'テスト', 0, 50000);
 
+-- 家計簿アーカイブテーブルを作成
+CREATE TABLE 家計簿アーカイブ (
+	日付 DATE NOT NULL,
+	費目ID INTEGER REFERENCES 費目(ID), -- 参照先を費目テーブルのIDを設定 外部キー制約
+	メモ NVARCHAR(100) DEFAULT '不明' NOT NULL,
+	入金額 INTEGER DEFAULT 0 CHECK(入金額 >= 0),
+	出金額 INTEGER DEFAULT 0 CHECK(出金額 >= 0),
+);
